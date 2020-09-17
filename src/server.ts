3409c81d9a4653d10d3312d4c30c7dca879d6109
galleonpt <danadotolinho@gmail.com>
2020-09-17 22:09:58 +0100

    Confiiguracao da database

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,16 @@ import { Server } from '@overnightjs/core';
 import { Application } from 'express';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecastController';
-
+import * as database from '@src/database';
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
   //configs do express
@@ -23,6 +24,14 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   //criar um server para os nossos testes
