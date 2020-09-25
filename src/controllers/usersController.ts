18cfd3ab143a6d6a628b0ee3f0136cec661ab6f2
galleonpt <danadotolinho@gmail.com>
2020-09-25 03:13:30 +0100

    Add: user validation

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,13 +1,18 @@
 import { Controller, Post } from '@overnightjs/core';
-import { Response, Request } from 'express';
+import { Response, Request, response } from 'express';
 import { User } from '@src/models/user';
+import { BaseController } from '.';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
-  public async create(req: Request, res: Response): Promise<void> {
-    const user = new User(req.body);
-    const newUser = await user.save();
-    res.status(201).send(newUser);
+  public async create(request: Request, response: Response): Promise<void> {
+    try {
+      const user = new User(request.body);
+      const newUser = await user.save();
+      response.status(201).send(newUser);
+    } catch (err) {
+      this.sendCreateUpdateErrorResponse(response, err);
+    }
   }
 }
