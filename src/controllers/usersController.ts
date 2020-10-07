46063bc4c6ba9ef54d674f48421dc9a2b9784562
galleonpt <danadotolinho@gmail.com>
2020-10-07 02:02:27 +0100

    Add: me route to decode token

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,9 +1,9 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
-import APIError from '@src/util/errors/api-error';
+import { AuthMiddleware } from '@src/middleware/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -40,5 +40,21 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     return response.status(200).send({ token: token });
+  }
+
+  @Get('me')
+  @Middleware(AuthMiddleware)
+  public async Me(request: Request, response: Response): Promise<Response> {
+    const email = request.decoded ? request.decoded.email : undefined;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return this.sendErrorResponse(response, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+    return response.send({ user });
   }
 }
